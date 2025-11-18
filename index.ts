1582805dc8305a7d63e5f166e419d28a1caee248
Eduart2021 <zeedymarina@yahoo.com>
2025-11-18 14:33:32 -0600

    adding async await

--- index.ts
+++ index.ts
@@ -4,29 +4,45 @@ import { fetchProductCatalog,
  } from './apiSimulator';
 
  // get all
-fetchProductCatalog()
-    .then((data)=>{
-        console.log(data);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
+ async function getProduct() {
+    
+   await fetchProductCatalog()
+     .then((data)=>{
+         console.log(data);
+        })
+        .catch((err)=>{
+            console.log('Error in get all data api ',err);
+        })
+        .finally(()=> console.log("Get All data from api is complete."))
+}
+getProduct()
+
 // get by id
-fetchProductReviews(2)
+async function productReport(params:number) {
+    
+   await fetchProductReviews(params)
     .then((res)=>{
         console.log(res);
     })
     .catch((err)=>{
-        console.log(err);
+        console.log('Error in get by id api ', err);
     })
+    .finally(()=> console.log("Get data by ID api is complete."))
+}
+productReport(2)
+
 // get sale by id
-fetchSalesReport('Book')
+async function saleReport(params:string) {
+    
+   await fetchSalesReport(params)
     .then((res)=>{
         res.map((res)=> {
             console.log(`Book sold: ${res.unitSale}, with a price of ${res.price}`)
         })
     })
     .catch((err)=>{
-        console.log(err);
+        console.log('Error in sale report api ',err);
     })
-
+    .finally(()=> console.log("Get sale by item name report from api is complete."))
+}
+saleReport('Book')
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Promises and Error Handling Challenge
+You are developing an e-commerce dashboard that fetches data from various APIs, including a product catalog, user reviews, and a sales report. Each API call could potentially fail due to network issues, incorrect endpoints, or data inconsistencies. To build a stable and user-friendly application, you need to manage these scenarios with proper error handling.
+
+Critical Thinking Questions
+Why is it important to handle errors for each individual API call rather than just at the end of the promise chain?
+How does using custom error classes improve debugging and error identification?
+When might a retry mechanism be more effective than an immediate failure response?
